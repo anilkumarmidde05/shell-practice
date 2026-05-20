33c903c2c5309322f3efffb061cc7729e67882cf
AnilKumar M <anilkumarmidde05@gmail.com>
2026-05-20 12:08:09 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:: Hello Ramesh, How are you?"
+echo "Ramesh:: I am doing good, how are you doing?"
+echo "Suresh:: I am doing good. What are you learning?"
+echo "Ramesh:: I am learning shell-scription"
