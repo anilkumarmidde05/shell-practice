bf4419ef54bdbe0ef5908ef393eb7916bc02ee8a
AnilKumar M <anilkumarmidde05@gmail.com>
2026-05-23 18:33:02 +0530

    errorhandigling@@

--- 10-installation.sh
+++ 10-installation.sh
@@ -7,5 +7,11 @@ if [ $USERID -ne 0 ]; then
    exit 1
 fi
 
-echo "installing nginx"
+echo "installing nginxxx"
 dnf install nginx -y
+if [ $? -ne 0 ]; then
+   echo "installing ngix ...failure"
+   exit 1
+else
+   echo "Installing Nginx ....success"
+fi
