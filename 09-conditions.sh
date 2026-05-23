a44d773f95cb7c996f5262e960e08438df511f56
AnilKumar M <anilkumarmidde05@gmail.com>
2026-05-23 16:49:31 +0530

    conditions@

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,10 +5,10 @@ NUM1=$1
 # -lt--> less than
 # -eq-->equal
 # -ne-->not equal
-if [$NUM1 -gt 20]; then
-echo "Given number : $NUM1 is greater than 20"
-elif [$NUM1 -eq 20];  then
-echo "Given number : $NUM1 is equal to 20"
+if [ $NUM1 -gt 20 ]; then
+    echo "Given number : $NUM1 is greater than 20"
+elif [ $NUM1 -eq 20 ];  then
+    echo "Given number : $NUM1 is equal to 20"
 else 
-echo "Given number : $NUM1 is less than 20"
+     echo "Given number : $NUM1 is less than 20"
 fi
