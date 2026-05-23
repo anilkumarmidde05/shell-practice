0e570cd6316f2a9888e0e8558a9e253843924e6c
AnilKumar M <anilkumarmidde05@gmail.com>
2026-05-23 06:35:16 +0530

    new variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 
-echo "Suresh:: Hello Ramesh, How are you?"
-echo "Ramesh:: I am doing good, how are you doing?"
-echo "Suresh:: I am doing good. What are you learning?"
-echo "Ramesh:: I am learning shell-scription"
+#echo "Suresh:: Hello Ramesh, How are you?"
+#echo "Ramesh:: I am doing good, how are you doing?"
+#echo "Suresh:: I am doing good. What are you learning?"
+#echo "Ramesh:: I am learning shell-scription"
+#Persion1=Suresh
+#Person2=Ramesh
+
+#echo "$Person1:: Hello $Person2, How are you?"
+#echo "$Person2:: I am doing good, how are you doing?"
+#echo "$Person1:: I am doing good. What are you learning?"
+#echo "$Person2:: I am learning shell-scription"
+
+Persion1=Anil
+Person2=Anu
+
+echo "$Person1:: Hello $Person2, How are you?"
+echo "$Person2:: I am doing good, how are you doing?"
+echo "$Person1:: I am doing good. What are you learning?"
+echo "$Person2:: I am learning shell-scription"
