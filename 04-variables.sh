aa755d52da20cffe9057c23dca5efc108eb845b6
AnilKumar M <anilkumarmidde05@gmail.com>
2026-05-23 06:58:16 +0530

    new arguments

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1:: Hello $Person2, How are you?"
+echo "$Person2:: I am doing good, how are you doing?"
+echo "$Person1:: I am doing good. What are you learning?"
+echo "$Person2:: I am learning shell-scription"
