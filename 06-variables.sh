00edf391ff7e3558389ec0a7d127879b3d58856f
AnilKumar M <anilkumarmidde05@gmail.com>
2026-05-23 14:33:57 +0530

    timeconversion

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+#TIMESTAMP=$(date)
 
-echo "script exectuted at: $TIMESTAMP"
+#echo "script exectuted at: $TIMESTAMP"
+
+###########################
+
+START_TIME=$(date +%s)
+echo "script executed at: $START_TIME"
+ 
+ sleep 10
+ END_TIME=$(date +%s)
+TOTAl_TIME=$(($END_TIME-$START_TIME))
+echo "total time is: $TOTAL_TIME seconds"
