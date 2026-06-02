d12996e81548190dae4e0e17d4892589f179dd39
AnilKumar M <anilkumarmidde05@gmail.com>
2026-06-02 16:08:05 +0530

    mailtemplate

--- mail.sh
+++ mail.sh
@@ -6,14 +6,14 @@ MESSAGE_BODY=$3
 ALERT_TYPE=$4
 SERVER_IP=$5
 TO_TEAM=$6
-
+SCRIPT_DIR=$PWD
 
 FINAL_MESSAGE=$(sed \
 -e "s|TO_TEAM|$TO_TEAM|g" \
 -e "s|ALERT_TYPE|$ALERT_TYPE|g" \
 -e "s|Server_IP|$SERVER_IP|g" \
 -e "s|Message|$MESSAGE_BODY|g" \
-template.html)
+$Script_DIR/template.html)
 
 
 
