03dd9fb1711b21c347e32f91ad32104948e5d579
AnilKumar M <anilkumarmidde05@gmail.com>
2026-06-02 13:14:15 +0530

    diskusage

--- 26-diskusage.sh
+++ 26-diskusage.sh
@@ -14,7 +14,7 @@ do
    USAGE= $(echo $line | awk '{print$6}' | cut -d "%" -f1)
    PARTITION= $(echo $line | awk '{print$7}')
    
-    if [ "$USAGE" -ge "$DISK_THRESHOLD" ]; then
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
        MESSAGE+="HIGH DISK usage on $PARTITION:$USAGE% \n"
     fi
 
