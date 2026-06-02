45ae2404049e8280be9508cc49f93353ce3aaf37
AnilKumar M <anilkumarmidde05@gmail.com>
2026-06-02 13:11:08 +0530

    diskusage

--- 26-diskusage.sh
+++ 26-diskusage.sh
@@ -9,7 +9,7 @@ MESSAGE=""
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=3
 
-while IFS read -r line
+while IFS= read -r line
 do
    USAGE= $(echo $line | awk '{print$6}' | cut -d "%" -f1)
    PARTITION= $(echo $line | awk '{print$7}')
